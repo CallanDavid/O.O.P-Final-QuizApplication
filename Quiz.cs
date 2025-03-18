1065dde9daefa20f107d790dca8228af8f396de6
Callan Merifield <callan.merifield@gmail.com>
2025-03-18 10:34:35 +0200

    'this' keyword

---
+++ Quiz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQuizApp
+{
+    internal class Quiz
+    {
+        private Question[] questions;
+
+        public Quiz(Question[] questions)
+        {
+            this.questions = questions;
+        }
+
+    }
+}
