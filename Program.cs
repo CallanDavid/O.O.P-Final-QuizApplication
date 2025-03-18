b2a89018e876914bfcdc708da394003e44658aba
Callan Merifield <callan.merifield@gmail.com>
2025-03-18 10:46:35 +0200

    Displaying Questions

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Adv.QuizAppTest
+﻿using MyQuizApp;
+using System.ComponentModel.DataAnnotations;
+
+namespace Adv.QuizAppTest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Question[] questions = new Question[]
+            {
+                new Question("What is the capital of Germany?",  // passing Question Text
+                new string[] { "Paris", "Berlin", "London", "Madrid" },     // passing Answers Array
+                1)  // CorrectAnswerIndex
+            };
+
+            Quiz myQuiz = new Quiz(questions);
+            myQuiz.DisplayQuestion(questions[0]);
+
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
